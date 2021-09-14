6e2ee4ff71d2b0bc246bb2355e8ec62d8cc48bf5
rajshetye11 <rajshetye11@gmail.com>
2021-09-15 00:58:43 +0530

    [raj] : added ability to load csv data for states

--- src/test/java/com/bridgelabz/indianstatescensus/IndianCensusTest.java
+++ src/test/java/com/bridgelabz/indianstatescensus/IndianCensusTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class IndianCensusTest {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\RAJ\\eclipsenewworkspace\\IndianStatesCensus\\lib\\IndiaStateCensusData.csv";
 	public IndianCensusAnalyser analyser;
+	private static final String INDIAN_CSV_STATE_PATH = "C:\\Users\\RAJ\\eclipsenewworkspace\\IndianStatesCensus\\lib\\StateCode.csv";
 	
 	@Test
 	public void returnSize() throws Exception {
@@ -52,6 +53,59 @@ public class IndianCensusTest {
 		try {
 			analyser = new IndianCensusAnalyser();
 			Assert.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
+		}catch(CustomException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+//	State
+
+	@Test
+	public void returnSizeState() throws Exception {
+		try {
+			analyser = new IndianCensusAnalyser();
+			Assert.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+		}catch(CustomException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void shouldExecuteProperly_IfRecordsAreEqualState() throws Exception {
+		try {
+			analyser = new IndianCensusAnalyser();
+			Assert.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+		}catch(CustomException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void shouldReturnRecordsif_CSVFileIsDetectedState() throws Exception {
+		try {
+			analyser = new IndianCensusAnalyser();
+			Assert.assertEquals(37, analyser.loadStateData("xyz.csv"));
+		}catch(CustomException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void shouldReturnCustomException_ifDelimiterIncorrectState() throws Exception {
+		try {
+			analyser = new IndianCensusAnalyser();
+			Assert.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+		}catch(CustomException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void shouldReturCustomException_ifCSVHeaderIncorrectState() throws Exception {
+		try {
+			analyser = new IndianCensusAnalyser();
+			Assert.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
 		}catch(CustomException e) {
 			e.printStackTrace();
 		}
